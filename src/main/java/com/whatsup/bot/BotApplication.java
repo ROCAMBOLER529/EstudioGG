87482b1396851581e5634fa1c4a555fa4be275bf
GonzaloFernandoA <gonzalofernandoa@gmail.com>
2024-10-29 20:30:58 -0300

    remove lombok

--- src/main/java/com/whatsup/bot/BotApplication.java
+++ src/main/java/com/whatsup/bot/BotApplication.java
@@ -1,17 +1,17 @@
 package com.whatsup.bot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import lombok.extern.slf4j.Slf4j;
-
 @SpringBootApplication
-@Slf4j
 public class BotApplication implements CommandLineRunner { 
 
+	private Logger log = LoggerFactory.getLogger(BotApplication.class);
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BotApplication.class, args);
--- src/main/java/com/whatsup/bot/controler/botControler.java
+++ src/main/java/com/whatsup/bot/controler/botControler.java
@@ -1,10 +1,14 @@
 package com.whatsup.bot.controler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.whatsup.bot.BotApplication;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -12,11 +16,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class botControler {
   
+	private Logger log = LoggerFactory.getLogger(botControler.class);
 	   
 	   @GetMapping("/hello")
 	    public ResponseEntity<String> exportOpportunity()  {
-	    	
-	
 	    	
 	        return new ResponseEntity<>("Hello", HttpStatus.OK );
 	    }
